caadd6cac3ca74df1d75b24f3dc87ab286ace543
Mandoo <leejuhyeon86@gmail.com>
2023-01-27 16:58:06 +0900

    just for pull

--- front/pages/home.js
+++ front/pages/home.js
@@ -1,34 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { CookiesProvider,useCookies } from 'react-cookie';
-import Link from 'next/link';
+import { CookiesProvider,useCookies } from 'react-cookie'
+import Link from 'next/link'
+import FixedBottomNavigation from './bottomNavFixed'
 
 export default function Home() {
     const [cookie, setCookie] = useCookies(['id']);
   return (
-    <>
-    <div className={styles.container}>
-      {cookie.name}
-    </div>
+    <div>
+      <head>
+        <title> ???서비스이름??? </title>
+      </head>
 
-  <head>
-    <title> ???서비스이름??? </title>
-  </head>
-
-  <body>
-    <p> 만나서 반가워요. </p>
-    <p> 김수연 님에 대해 알고 싶어요. </p>
-    <p> 나는 최대 </p> <p> 시간 걸을 수 있어요. </p>
-    <p> 나는 </p> <p> 이 있어요. </p>
-    <p> 나의 음식 성향은 </p> <p> 이에요. </p>
-    <p> 나는 </p>
-    <button> 확인 </button>
-  </body>
-
-  <div>
-    <FixedBottomNavigation/>
+      
   </div>
-  </>
   )
 }
